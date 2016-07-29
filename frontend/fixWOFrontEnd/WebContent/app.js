61dcfbde47b8a91d1db951c462716dadd1218fd2
ArthurFP <arthurfp@sistemas.ufla.br>
2016-07-29 06:25:02 -0300

    Adição de Mapa Proveniente de Busca

--- frontend/fixWOFrontEnd/WebContent/app.js
+++ frontend/fixWOFrontEnd/WebContent/app.js
@@ -1,8 +1,15 @@
-var app = angular.module('fixWO', ['jsonService']);
+var app = angular.module('fixWO', ['jsonService','searchMapJsonService']);
 
 app.controller('Main', function($scope, JsonService) {
-  JsonService.get(function(data){
-    $scope.estado = data.estado;
-    $scope.cidades = data.cidades;
-  });
-});
+	  JsonService.get(function(data){
+	    $scope.estado = data.estado;
+	    $scope.cidades = data.cidades;
+	  });
+	});
+
+app.controller('Mapa', function($scope, SearchMapJsonService) {
+	SearchMapJsonService.get(function(search){
+	    $scope.searchQuery = search.searchQuery;
+	    $scope.locais = search.locais;
+	  });
+	});
--- frontend/fixWOFrontEnd/WebContent/services.js
+++ frontend/fixWOFrontEnd/WebContent/services.js
@@ -2,3 +2,8 @@ angular.module('jsonService', ['ngResource'])
 .factory('JsonService', function($resource) {
   return $resource('data.json');
 });
+
+angular.module('searchMapJsonService', ['ngResource'])
+.factory('SearchMapJsonService', function($resource) {
+  return $resource('search.json');
+});
