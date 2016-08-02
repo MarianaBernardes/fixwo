4a7e6e85a0feb6e432455cabc94c3492a006e56e
Pedro Mutter <pedro.mutter@gmail.com>
2016-08-02 16:01:40 -0300

    Integrando EasyBPMS
    correções dos testes iniciais

--- backend/processo/fixWO/src/main/java/Main.java
+++ backend/processo/fixWO/src/main/java/Main.java
@@ -4,9 +4,9 @@ import java.util.List;
 import com.easybpms.bd.CRUDException;
 import com.easybpms.bd.ScriptBd;
 import com.easybpms.bd.dao.CRUDUser;
-import com.easybpms.bd.dao.CRUDUserGroup;
+import com.fixo.DAO.CRUDGrupo;
 import com.fixo.DAO.CRUDOcorrencia;
-import com.fixwo.domain.Grupo;
+import com.fixo.DAO.CRUDUsuario;
 import com.fixwo.domain.Ocorrencia;
 import com.fixwo.domain.Usuario;
 
@@ -14,92 +14,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		/*
-		 * Lembrar de criar outros grupos
-		 */
-		/*Grupo grupo = new Grupo();
-		grupo.setName("Triador");
+		// Criação de grupos
+		CRUDGrupo grupo1 = new CRUDGrupo("Triador");
+		CRUDGrupo grupo2 = new CRUDGrupo("Usuario");
+		CRUDGrupo grupo3 = new CRUDGrupo("ResponsavelArea");
+		CRUDGrupo grupo4 = new CRUDGrupo("Funcionario");
 		
-		try {
-			CRUDUserGroup.create(grupo);
-		} catch (CRUDException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		Grupo grupo2 = new Grupo();
-		grupo2.setName("Usuario");
-		
-		try {
-			CRUDUserGroup.create(grupo2);
-		} catch (CRUDException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		Grupo grupo3 = new Grupo();
-		grupo3.setName("ResponsavelArea");
-		
-		try {
-			CRUDUserGroup.create(grupo3);
-		} catch (CRUDException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		// criação de usuários
-		Usuario usu = new Usuario();
-		usu.setId("1");
-		usu.setName("Jean");
-		usu.setTenancy("1"); //id do cliente 1=fixo
-		
-		List<String> li = new ArrayList();
-		li.add("Triador");
-		
-		usu.setUserGroupNames(li);
-		
-		try {
-			CRUDUser.create(usu);
-		} catch (CRUDException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		Usuario usu2 = new Usuario();
-		usu2.setId("2");
-		usu2.setName("Roger");
-		usu2.setTenancy("1"); //id do cliente 1=fixo
-		
-		List<String> li2 = new ArrayList();
-		li2.add("ResponsavelArea");
-		
-		usu2.setUserGroupNames(li2);
-		
-		try {
-			CRUDUser.create(usu2);
-		} catch (CRUDException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		
-		Usuario usu3 = new Usuario();
-		usu3.setId("3");
-		usu3.setName("Pedro");
-		usu3.setTenancy("1"); //id do cliente 1=fixo
-		
-		List<String> li3 = new ArrayList();
-		li3.add("Usuario");
-		
-		usu3.setUserGroupNames(li3);
-		
-		try {
-			CRUDUser.create(usu3);
-		} catch (CRUDException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+		// Criação de usuários
+		CRUDUsuario usu1 = new CRUDUsuario("1", "Jean", "1", "Triador");
+		CRUDUsuario usu2 = new CRUDUsuario("2", "Roger", "1", "ResponsavelArea");
+		CRUDUsuario usu3 = new CRUDUsuario("3", "Pedro", "1", "Usuario");
+		CRUDUsuario usu4 = new CRUDUsuario("4", "Mariana", "1", "Funcionario");
 	
 		// Cadastro de ocorrência
 		Ocorrencia o = new Ocorrencia();
---
+++ backend/processo/fixWO/src/main/java/com/fixo/DAO/CRUDUsuario.java
@@ -0,0 +1,32 @@
+package com.fixo.DAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.easybpms.bd.CRUDException;
+import com.easybpms.bd.dao.CRUDUser;
+import com.fixwo.domain.Usuario;
+
+public class CRUDUsuario {
+
+	public CRUDUsuario(String id, String nome, String tenancy, String grupo) {
+		Usuario usu = new Usuario();
+		usu.setId(id);
+		usu.setName(nome);
+		usu.setTenancy(tenancy);
+
+		List<String> li = new ArrayList<String>();
+		li.add(grupo);
+
+		usu.setUserGroupNames(li);
+
+		try {
+			CRUDUser.create(usu);
+		} catch (CRUDException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
