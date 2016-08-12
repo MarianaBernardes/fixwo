2c25f06fee3185840fe45416603184caaef2e0e3
= <benhur.nascimento@gmail.com>
2016-08-12 17:05:06 -0300

    Script de dependencias criado para facilitar a vida (aparentemente o ionic devia gerenciar isso mas estamos tendo problemas...)

---
+++ mobile/fixwo/dependencias.sh
@@ -0,0 +1,6 @@
+ionic plugin add cordova-plugin-camera
+ionic plugin add cordova-sqlite-storage
+ionic plugin add cordova-plugin-file
+ionic plugin add cordova-plugin-filepath
+ionic plugin add cordova-plugin-statusbar
+ionic plugin add https://github.com/brodysoft/Cordova-SQLitePlugin.git
