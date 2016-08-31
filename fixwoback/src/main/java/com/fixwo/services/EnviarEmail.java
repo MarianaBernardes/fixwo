4af13413ad52c870723b96c5a7cc95d62947dc58
Mariana Bernardes <mari.bernardes93@gmail.com>
2016-08-31 18:40:42 -0300

    Processo de negócio fixwo mais completo

---
+++ fixwoback/src/main/java/com/fixwo/services/EnviarEmail.java
@@ -0,0 +1,13 @@
+package com.fixwo.services;
+
+public class EnviarEmail {
+	
+	public void run(){
+		/*Receber o id da ocorrencia para conseguir buscar o usuario que solicitou 
+		  a ocorrencia. Enviar um email para o usuario avisando que nao eh possivel
+		  registrar a ocorrencia pois a area nao esta cadastrada e consequentemente
+		  nao existe cliente responsavel*/
+	}
+
+
+}
