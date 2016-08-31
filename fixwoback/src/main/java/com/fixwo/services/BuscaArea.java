1b0873336dd67af621229e393be2569ecdc1f1d3
Mariana Bernardes <mari.bernardes93@gmail.com>
2016-08-31 10:08:17 -0300

    Add branch fixwo para testes

--- fixwoback/src/main/java/com/fixwo/services/BuscaArea.java
+++ fixwoback/src/main/java/com/fixwo/services/BuscaArea.java
@@ -3,7 +3,12 @@ package com.fixwo.services;
 public class BuscaArea {
 
 	public void run() {
-		System.out.println("Serviço buscar área");
+		/*Receber o id da ocorrencia para conseguir buscar a geolocalizacao armazenada
+		para a ocorrencia. Fazer uma busca das areas cadastradas com aquelas coordenadas.
+		Extrair a area mais proxima e setar ela na ocorrencia. Se for encontrada atribuir 
+		true para o parametro existeArea do processo ou false caso nao seja encontrada e 
+		retornar esse valor*/
+		System.out.println("Servico buscar area");
 	}
 
 }
